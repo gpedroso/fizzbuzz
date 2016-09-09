c3f5904e495048b81deb2c8156cde1ec97b976b4
Gabriel Pedroso <gpedroso@gmail.com>
2016-09-08 21:23:23 -0300

    unittests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,6 +9,7 @@ Regras do FizzBuzz
 """
 from functools import partial
 
+
 multiple_of = lambda base, num: num % base == 0
 multiple_of_3 = partial(multiple_of,3)
 multiple_of_5 = partial(multiple_of,5)
@@ -24,20 +25,3 @@ def robot(pos):
         say = 'buzz'
 
     return say
-
-if __name__ == '__main__':
-    assert(robot(1) == '1')
-    assert(robot(2) == '2')
-    assert(robot(4) == '4')
-
-    assert(robot(3) == 'fizz')
-    assert(robot(6) == 'fizz')
-    assert(robot(9) == 'fizz')
-
-    assert(robot(5) == 'buzz')
-    assert(robot(10) == 'buzz')
-    assert(robot(20) == 'buzz')
-
-    assert(robot(15) == 'fizzbuzz')
-    assert(robot(30) == 'fizzbuzz')
-    assert(robot(45) == 'fizzbuzz')
